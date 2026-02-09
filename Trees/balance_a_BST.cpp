7f666afcd72688443d864c1cc087b4035063de97
Prashanth2902 <cbeprashanth@gmail.com>
2026-02-08 19:11:42 -0500

    LC 1382

---
+++ Trees/balance_a_BST.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/**
+ * Leetcode Medium
+ * Problem - 1382. Balance a Binary Search Tree
+ * Link -> https://leetcode.com/problems/balance-a-binary-search-tree/description/?envType=daily-question&envId=2026-02-09
+ * 
+ * Logic:-
+ * Since its already a BST. The inorder traversal will be sorted.
+ * then do divide and conquer with sorted array to get balanced BST
+ */
+
+
+
+  //Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+
+class Solution {
+public:
+    vector<TreeNode*> sortedArr;
+
+    void inorderTraverse(TreeNode* root) {
+        if (root == NULL) return;
+        inorderTraverse(root->left);
+        sortedArr.push_back(root);
+        inorderTraverse(root->right);
+    }
+    
+    TreeNode* sortedArrayToBST(int start, int end) {
+        if (start > end) return NULL;
+        int mid = (start + end) / 2;
+        TreeNode* root = sortedArr[mid];
+        root->left = sortedArrayToBST(start, mid - 1);
+        root->right = sortedArrayToBST(mid + 1, end);
+        return root;
+    }
+
+    TreeNode* balanceBST(TreeNode* root) {
+        inorderTraverse(root);
+        return sortedArrayToBST(0, sortedArr.size() - 1);
+    }
+
+
+};
